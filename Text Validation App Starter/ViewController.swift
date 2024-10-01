20cb9d20db7ff10ae4000e3f7399a52d8ebc17ae
janemadsen <janemadsen815@gmail.com>
2024-10-01 10:14:34 -0600

    Initial Commit

---
+++ Text Validation App Starter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text Validation App Starter
+//
+//  Created by Jane Madsen on 10/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
